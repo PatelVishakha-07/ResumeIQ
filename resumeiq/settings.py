a555a3cafe2cb4b1f9415b3170db423597db0cac
aayushi1944 <patelayushi1928@gmail.com>
2026-08-21 14:36:32 +0530

    login & registration UI done

--- resumeiq/settings.py
+++ resumeiq/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'resumeiq.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ resumeiq/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def login_view(request):
+    return render(request, 'authentication/login.html')
+
+def register_view(request):
+    return render(request, 'authentication/register.html')
