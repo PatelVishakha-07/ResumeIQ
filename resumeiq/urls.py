ec217f2060c6d7dbf8e2727bd1e55075aadfe07c
PatelVishakha-07 <vishakhapatel8270@gmail.com>
2026-08-21 15:34:37 +0530

    home page UI done

--- resumeiq/urls.py
+++ resumeiq/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [
+urlpatterns = [    
     path('admin/', admin.site.urls),
+    path('', views.home_view, name = 'home'),
     path('login/',views.login_view, name= 'login'),
     path('register/', views.register_view, name='register'),
 
--- resumeiq/views.py
+++ resumeiq/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def home_view(request):
+    return render(request, "home.html")
+
 def login_view(request):
     return render(request, 'authentication/login.html')
 
