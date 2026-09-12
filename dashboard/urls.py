7d663ab891b6caaa54f64b42315ebef7953cc986
aayushi1944 <patelayushi1928@gmail.com>
2026-09-13 00:48:39 +0530

    Admin dashboard UI done

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard_redirect, name="dashboard"),
+    path('admin_panel/', views.dashboard_redirect, name="dashboard"),
+    path('admin_panel/overview',views.adminOverview, name="adminOverview"),
+    path('admin_panel/manage_user', views.manageUsers, name="manageUser"),
+    path('admin_panel/manage_user/<int:user_id>/toggle/',views.toggleUserStatus,name='toggle_user_status'),
+    path('admin_panel/manage_staffRole', views.manageStaffRole, name="manageStaffRole"),
+    path('admin_panel/feedback', views.feedback, name="feedback"),
 ]
