36c0552500d6b0386650b67bd7542e98509c07f4
PatelVishakha-07 <vishakhapatel8270@gmail.com>
2026-09-12 23:09:10 +0530

    msg: user side resume upload and history

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard_redirect, name="dashboard"),
+    path('ats_score_generator/', views.ats_score_generator_view, name="ats_score_generator"),
+    path("resume_history/", views.resume_history_view, name="resume_history"),
 ]
